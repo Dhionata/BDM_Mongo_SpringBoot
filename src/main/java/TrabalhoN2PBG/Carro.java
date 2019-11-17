15b8f301fcd3c7cfef18622e62d7ce346f6fe488
Dhionata <xiyunagi@gmail.com>
2019-11-17 19:33:36 -0300

    1.2 Testando Lombok @Data

--- src/main/java/TrabalhoN2PBG/Carro.java
+++ src/main/java/TrabalhoN2PBG/Carro.java
@@ -2,53 +2,13 @@ package TrabalhoN2PBG;
 
 import org.springframework.data.annotation.Id;
 
+import lombok.Data;
+
+@Data
 public class Carro {
     @Id
     private int id;
     private String modelo;
     private String marca;
     private String cor;
-
-    public Carro() {
-
-    }
-
-    public Carro(String modelo, String marca, String cor) {
-	setModelo(modelo);
-	setMarca(marca);
-	setCor(cor);
-    }
-
-    // Getters e Setters (Melhor usar @Data do Lombok!)
-    public int getId() {
-	return id;
-    }
-
-    public void setId(int id) {
-	this.id = id;
-    }
-
-    public String getModelo() {
-	return modelo;
-    }
-
-    public void setModelo(String modelo) {
-	this.modelo = modelo;
-    }
-
-    public String getMarca() {
-	return marca;
-    }
-
-    public void setMarca(String marca) {
-	this.marca = marca;
-    }
-
-    public String getCor() {
-	return cor;
-    }
-
-    public void setCor(String cor) {
-	this.cor = cor;
-    }
 }
