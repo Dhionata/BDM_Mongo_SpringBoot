d37fc16c4336469c192c74d8af517f22c13db6d1
Dhionata <xiyunagi@gmail.com>
2019-11-17 19:56:26 -0300

    1.2.4 Adicionado a Classe-Main "App" no pacote de exemplo

---
+++ src/main/java/Aula08/HellouSpringBoot/App.java
@@ -0,0 +1,13 @@
+package Aula08.HellouSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+
+    public static void main(String[] args) {
+	System.out.println("Iniciando Spring Boot...");
+	SpringApplication.run(App.class, args);
+    }
+}
